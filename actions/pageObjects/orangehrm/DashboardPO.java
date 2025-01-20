a6c74d9c44097bc7d29ba45d210d5a10aeae8a59
Nhn2025 <dnhu122025@gmail.com>
2025-01-20 11:53:44 +0700

    orangeHRM-5 format code

--- actions/pageObjects/orangehrm/DashboardPO.java
+++ actions/pageObjects/orangehrm/DashboardPO.java
@@ -1,5 +1,6 @@
 package pageObjects.orangehrm;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import pageObjects.orangehrm.pim.employee.SideBarTabsPO;
 import pageUIs.orangeHRM.DashboardPUI;
@@ -12,6 +13,7 @@ public class DashboardPO extends SideBarTabsPO {
         this.driver = driver;
     }
 
+    @Step("Dashboard text is displayed")
     public boolean isDashboardTextDisplayed() {
         waitForElementVisible(driver, DashboardPUI.DASHBOARD_TEXT);
         return isElementDisplayed(driver, DashboardPUI.DASHBOARD_TEXT);
